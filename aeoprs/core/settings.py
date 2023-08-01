c2d17ddb74bc4dad2576ed5c905bb625ffd6e858
Sylvain Gaudan <gaudan@sylvains-air.lan>
2023-08-01 15:59:16 +0200

    add entry point

--- aeoprs/core/settings.py
+++ aeoprs/core/settings.py
@@ -1,24 +1,26 @@
 from enum import Enum
 
-import yaml
 from pydantic import BaseModel, Extra, Field
+from envyaml import EnvYAML
+from aeoprs.logger import CustomLogger as Logger
 
+LOGGER = Logger.get_logger()
 
 class S3(BaseModel):
     access_key_id:str
     secret_access_key:str
-    platform:str=None
+    platform:str=Field(None)
     tier:str="Standard"
-    region:str
-    endpoint_url:str
+    region:str=Field(None)
     asset_http_endpoint_url:str
+    endpoint_url:str
     bucket:str
 
 class Index(BaseModel, extra=Extra.allow):
     collection_prefix:str
     endpoint_url:str
-    login:str=None
-    pwd:str=None
+    login:str=Field(None)
+    pwd:str=Field(None)
 
 class Settings(BaseModel, extra=Extra.allow):
     s3:S3
@@ -29,6 +31,6 @@ class Configuration:
 
     @staticmethod
     def init(configuration_file:str):
-        with open(configuration_file) as file:
-            Configuration.settings=Settings.parse_obj(yaml.load(file, Loader=yaml.FullLoader))
-
+        envyaml=EnvYAML(configuration_file, strict=False)
+        Configuration.settings=Settings.parse_obj(envyaml)
+        print(Configuration.settings.dict())
