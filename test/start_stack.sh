9c7f5498e01747137c1dc4fe3167b4ae8411ea36
MatthieuBarbet <matthieu.barbet@gisaia.com>
2023-09-06 15:20:22 +0200

    Fix: create bucket in start stack

--- test/start_stack.sh
+++ test/start_stack.sh
@@ -1,9 +1,17 @@
-# Start ES and minio
+# Start  minio
+docker-compose -f test/docker-compose-es-minio.yaml up  -d minio
+#Waiting for minio service up and running
+code=""
+code_OK="OK"
+ while [[ "$code" != *$code_OK* ]];do
+    code="$(curl -IL --silent http://localhost:9000/minio/health/live | grep "^HTTP\/")"
+    eval "sleep 5"
+done
+# Start  create buckets
+docker-compose -f test/docker-compose-es-minio.yaml up -d createbuckets
+# Start  elastic rabbitmq redis
 docker-compose -f test/docker-compose-es-minio.yaml up -d
 
-
-# Wait a bit then launch the AEOPRS Service
-sleep 5
 export PYTHONPATH=`pwd`:`pwd`/extensions:`pwd`/test
 export AEOPROCESSES_CONFIGURATION_FILE=`pwd`/test/conf/aeoprocesses.yaml
 python3 aeoprs.py test/conf/aeoprs.yaml &
