8257df3820667e1270e554965baf992c87bdf780
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2023-09-26 13:42:01 +0200

    release images airs, aproc-proc and aproc-service

--- release/release.sh
+++ release/release.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
-IMAGE=airs
 [ -z "$1" ] && echo "Please provide the version" && exit 1;
 VERSION=$1
 echo "Build and releas the image with version ${VERSION}"
 
-echo "Building the image ..."
-docker build -t gisaia/${IMAGE}:${VERSION} -t gisaia/${IMAGE}:latest .
+build_and_publish_docker (){
+    IMAGE=$1
+    echo "Building the image ..."
+    docker build -f Dockerfile-${IMAGE} -t gisaia/${IMAGE}:${VERSION} -t gisaia/${IMAGE}:latest .
 
-echo "Publishing the image ..."
-docker login
-docker push gisaia/${IMAGE}:latest
-docker push gisaia/${IMAGE}:${VERSION}
+    echo "Publishing the image ..."
+    docker login
+    docker push gisaia/${IMAGE}:latest
+    docker push gisaia/${IMAGE}:${VERSION}
+}
 
+
+#---------------    APROC    ----------------
+build_and_publish_docker aproc-proc
+
+build_and_publish_docker aproc-service
+
+#---------------    AIRS    ----------------
+# DOCKER
+build_and_publish_docker airs
+exit
+
+# PYTHON PIP
 export PYTHONPATH=`pwd`:`pwd`/extensions:`pwd`/test
 python3 airs/core/models/utils.py > docs/model/model.schema.json
 jsonschema2md -d docs/model/ -o docs/model/
 ./publish/publish.sh $VERSION
 
+# DOCUMENTATION
 git add docs/
 git commit -m "update docs for version "$VERSION
 git push origin
 git tag -a ${VERSION} -m "ARLAS Item Registration Services Model ${VERSION}"
 git push origin ${VERSION}
+
