fa1e8bdceb01ed72272091190fecec744919c1e1
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2023-10-20 11:24:21 +0200

    add cors for other http method

--- aproc/cli/aproc.py
+++ aproc/cli/aproc.py
@@ -3,6 +3,8 @@ import os
 import typer
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
 
 from aproc.service.aproc_services import AprocServices
 from common.exception_handler import EXCEPTION_HANDLERS
@@ -12,6 +14,7 @@ cli = typer.Typer()
 APROC_HOST = os.getenv("APROC_HOST", "127.0.0.1")
 APROC_PORT = os.getenv("APROC_PORT", "8001")
 APROC_PREFIX = os.getenv("APROC_PREFIX", "/arlas/aproc")
+APROC_CORS = os.getenv("APROC_CORS", "*")
 
 
 @cli.command(help="Start the ARLAS Processing Service.")
@@ -20,7 +23,10 @@ def run(
         port: int = typer.Argument(default=APROC_PORT, help="port")
         ):
     app = FastAPI(version='0.0', title='ARLAS Processes',
-                  description='ARLAS Processes')
+                  description='ARLAS Processes',
+                  middleware=[
+                    Middleware(CORSMiddleware, allow_origins=APROC_CORS.split(","))
+                  ])
     app.include_router(ROUTER, prefix=APROC_PREFIX)
     for eh in EXCEPTION_HANDLERS:
         app.add_exception_handler(eh.exception, eh.handler)
