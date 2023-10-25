750d0af57571984282426fd420c374a162a64148
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2023-10-25 17:21:30 +0200

    file exploration starts at root

--- fam/core/fam.py
+++ fam/core/fam.py
@@ -3,27 +3,27 @@ import os
 from extensions.aproc.proc.ingest.drivers.drivers import Drivers
 from fam.core.model import Archive
 
+
 class Fam():
 
-    def list_archives(prefix: str, path: str, size: int = 0, max_size: int = 10) -> list[Archive]:
-        full_path=os.path.join(prefix, path)
+    def list_archives(path: str, size: int = 0, max_size: int = 10) -> list[Archive]:
         if size >= max_size or os.path.basename(path).startswith("."):
             return []
-        driver = Drivers.solve(full_path)
+        driver = Drivers.solve(path)
         if driver is not None:
-            archive = Archive(id=driver.get_item_id(full_path),
+            archive = Archive(id=driver.get_item_id(path),
                               name=os.path.basename(path),
                               driver_name=driver.name,
                               path=path,
-                              is_dir=os.path.isdir(full_path),
-                              last_modification_date=datetime.datetime.fromtimestamp(os.path.getmtime(full_path)),
-                              creation_date=datetime.datetime.fromtimestamp(os.path.getctime(full_path)))
+                              is_dir=os.path.isdir(path),
+                              last_modification_date=datetime.datetime.fromtimestamp(os.path.getmtime(path)),
+                              creation_date=datetime.datetime.fromtimestamp(os.path.getctime(path)))
             return [archive]
         else:
-            if os.path.isdir(full_path):
+            if os.path.isdir(path):
                 archives: list[Archive] = []
-                for file in os.listdir(full_path):
-                    sub_archives = Fam.list_archives(prefix, os.path.join(path, file), size=size, max_size=max_size)
+                for file in os.listdir(path):
+                    sub_archives = Fam.list_archives(os.path.join(path, file), size=size, max_size=max_size)
                     size = size + len(sub_archives)
                     archives = archives + sub_archives
                 return archives
--- test/fam_tests.py
+++ test/fam_tests.py
@@ -6,7 +6,7 @@ import requests
 from fastapi import status
 from utils import FAM_URL
 
-from fam.core.model import Archive, PathRequest
+from fam.core.model import Archive, File, PathRequest
 
 
 class Tests(unittest.TestCase):
@@ -15,7 +15,10 @@ class Tests(unittest.TestCase):
         ...
 
     def test_not_found(self):
-        r = requests.post(url=os.path.join(FAM_URL, "files"), data=PathRequest(path="a_file_that_does_not_exist").model_dump_json(), headers={"Content-Type": "application/json"})
+        r = requests.get(url=os.path.join(FAM_URL, "root"))
+        print(r.content)
+        root: File = File(**r.json())
+        r = requests.post(url=os.path.join(FAM_URL, "files"), data=PathRequest(path=root.path + "/a_file_that_does_not_exist").model_dump_json(), headers={"Content-Type": "application/json"})
         self.assertEquals(r.status_code, status.HTTP_404_NOT_FOUND, r.content)
 
     def test_dot_dot_not_authorized(self):
@@ -23,15 +26,19 @@ class Tests(unittest.TestCase):
         self.assertEquals(r.status_code, status.HTTP_400_BAD_REQUEST, r.content)
 
     def test_directory(self):
-        r = requests.post(url=os.path.join(FAM_URL, "files"), data=PathRequest(path="DIMAP").model_dump_json(), headers={"Content-Type": "application/json"})
+        r = requests.get(url=os.path.join(FAM_URL, "root"))
+        root: File = File(**r.json())
+        r = requests.post(url=os.path.join(FAM_URL, "files"), data=PathRequest(path=root.path + "/DIMAP").model_dump_json(), headers={"Content-Type": "application/json"})
         self.assertTrue(r.ok)
 
     def test_archive(self):
-        r = requests.post(url=os.path.join(FAM_URL, "archives"), data=PathRequest(path="DIMAP").model_dump_json(), headers={"Content-Type": "application/json"})
+        r = requests.get(url=os.path.join(FAM_URL, "root"))
+        root: File = File(**r.json())
+        r = requests.post(url=os.path.join(FAM_URL, "archives"), data=PathRequest(path=root.path + "/DIMAP").model_dump_json(), headers={"Content-Type": "application/json"})
         self.assertTrue(r.ok)
         archive = Archive(**(json.loads(r.content)[0]))
         self.assertEquals(archive.name, "IMG_SPOT6_MS_001_A")
-        self.assertEquals(archive.path, "DIMAP/PROD_SPOT6_001/VOL_SPOT6_001_A/IMG_SPOT6_MS_001_A")
+        self.assertEquals(archive.path, root.path + "/DIMAP/PROD_SPOT6_001/VOL_SPOT6_001_A/IMG_SPOT6_MS_001_A")
         self.assertEquals(archive.is_dir, True)
         self.assertEquals(archive.id, "inputs-DIMAP-PROD_SPOT6_001-VOL_SPOT6_001_A-IMG_SPOT6_MS_001_A")
         self.assertEquals(archive.driver_name, "dimap")
