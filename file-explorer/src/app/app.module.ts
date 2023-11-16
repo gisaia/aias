6b294a321f7138d93767e59b81eba4477727ef3d
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2023-11-16 21:28:04 +0100

    feat: add custom translate loader
    update toolkit to beta.14

--- file-explorer/src/app/app.module.ts
+++ file-explorer/src/app/app.module.ts
@@ -1,14 +1,9 @@
 import { APP_INITIALIZER, NgModule, forwardRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { ArlasCollaborativesearchService, ArlasConfigurationDescriptor, ArlasIamService, ArlasStartupService, ArlasToolKitModule, ArlasToolkitSharedModule, AuthentificationService, CONFIG_UPDATER, FETCH_OPTIONS, GET_OPTIONS, LoginModule, ToolkitRoutingModule, configUpdaterFactory, getOptionsFactory } from 'arlas-wui-toolkit';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -20,33 +15,38 @@ import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
-import { RouterModule } from '@angular/router';
-import { StartupService } from './services/startup.service';
-import { OAuthModule } from 'angular-oauth2-oidc';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { ExplorerComponent } from './components/explorer/explorer.component';
-import { ArchivesComponent } from './components/archives/archives.component';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { TasksComponent } from './components/tasks/tasks.component';
-import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { RouterModule } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { ArlasTranslateLoader } from '@tools/customLoader';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import {
+  ArlasCollaborativesearchService, ArlasConfigurationDescriptor, ArlasIamService, ArlasSettingsService,
+  ArlasStartupService, ArlasToolkitSharedModule, AuthentificationService, CONFIG_UPDATER, FETCH_OPTIONS, GET_OPTIONS,
+  LoginModule, PersistenceService, configUpdaterFactory, getOptionsFactory
+} from 'arlas-wui-toolkit';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ToastrModule } from 'ngx-toastr';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ArchivesComponent } from './components/archives/archives.component';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { ExplorerComponent } from './components/explorer/explorer.component';
+import { HomeComponent } from './components/home/home.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { StartupService } from './services/startup.service';
 
 export function startupServiceFactory(startupService: StartupService) {
   const init = () => startupService.init();
   return init;
-}
-
-export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 }
 
 @NgModule({
@@ -98,8 +98,8 @@ export function createTranslateLoader(http: HttpClient) {
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [HttpClient]
+        useClass: ArlasTranslateLoader,
+        deps: [HttpClient, ArlasSettingsService, PersistenceService]
       }
     }),
     OAuthModule.forRoot()
