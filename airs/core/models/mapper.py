619247768dbee64f9f806bed9e2611882e194d4d
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2024-01-04 10:30:20 +0100

    date to int

--- airs/core/models/mapper.py
+++ airs/core/models/mapper.py
@@ -1,10 +1,17 @@
 import json
+import datetime
 
 from airs.core.models.model import Item
 
 
-def to_airs_item(item: Item)->Item:
-     """ Takes an item and makes sure to map the namespace fields to AIRS Item. Keys can contain : as namespace seperator
+def serialize_datetime(obj): 
+    if isinstance(obj, datetime.datetime):
+        return int(obj.timestamp())
+    raise TypeError("Type not serializable")
+
+
+def to_airs_item(item: Item) -> Item:
+    """ Takes an item and makes sure to map the namespace fields to AIRS Item. Keys can contain : as namespace seperator
 
      Args:
          item (Item): The item (with : namespace separator)
@@ -12,12 +19,12 @@ def to_airs_item(item: Item)->Item:
      Returns:
          item: The airs item
      """
-     dictionary=item.model_dump(exclude_unset=True, by_alias=True, exclude_none=True)
-     return Item(**__replaceKeys(dictionary, ":", "__"))
+    dictionary = item.model_dump(exclude_unset=True, by_alias=True, exclude_none=True)
+    return Item(**__replaceKeys(dictionary, ":", "__"))
 
 
-def to_dict(item: Item)->dict:
-     """ create a dictionnary from the Item. Keys contain : as namespace seperator
+def to_dict(item: Item) -> dict:
+    """ create a dictionnary from the Item. Keys contain : as namespace seperator
 
      Args:
          item (Item): The item
@@ -25,83 +32,90 @@ def to_dict(item: Item)->dict:
      Returns:
          dict: The dictionary. Keys contain : as namespace seperator
      """
-     dictionary=item.model_dump(exclude_unset=True, by_alias=True, exclude_none=True)
-     return __replaceKeys(dictionary, "__", ":")
+    dictionary = item.model_dump(exclude_unset=True, by_alias=True, exclude_none=True)
+    return __replaceKeys(dictionary, "__", ":")
 
-def to_airs_dict(item: Item)->dict:
-     """ create a dictionnary from the Item. Keys contain __ as namespace seperator
 
-     Args:
-         item (Item): The item
+def to_airs_dict(item: Item) -> dict:
+    """ create a dictionnary from the Item. Keys contain __ as namespace seperator
 
-     Returns:
-         dict: The dictionary. Keys contain __ as namespace seperator
-     """
-     dictionary=item.model_dump(exclude_unset=True, by_alias=False, exclude_none=True)
-     return dictionary
+    Args:
+        item (Item): The item
 
-def to_airs_json(item: Item)->str:
-     """ create a json from the Item. Keys contain __ as namespace seperator
+    Returns:
+        dict: The dictionary. Keys contain __ as namespace seperator
+    """
+    dictionary = item.model_dump(exclude_unset=True, by_alias=False, exclude_none=True)
+    return dictionary
 
-     Args:
-         item (Item): The item
 
-     Returns:
-         str: the json. Keys contain __ as namespace seperator
-     """
-     return json.dumps(to_airs_dict(item), default=str, indent=2)
+def to_airs_json(item: Item) -> str:
+    """ create a json from the Item. Keys contain __ as namespace seperator
 
-def to_json(item: Item)->str:
-     """ create a json from the Item. Keys contain : as namespace seperator
+    Args:
+        item (Item): The item
 
-     Args:
-         item (Item): The item
+    Returns:
+        str: the json. Keys contain __ as namespace seperator
+    """
+    return json.dumps(to_airs_dict(item), indent=2, default=serialize_datetime)
 
-     Returns:
-         str: the json. Keys contain : as namespace seperator
-     """
-     return json.dumps(to_dict(item), default=str, indent=2)
 
-def item_from_json_file(jsonfile)->Item:
-     """ load a json file containing an Item
+def to_json(item: Item) -> str:
+    """ create a json from the Item. Keys contain : as namespace seperator
 
-     Args:
-         jsonfile (file): the json file
+    Args:
+        item (Item): The item
 
-     Returns:
-         Item: _description_
-     """
-     return Item(**__replaceKeys(json.load(jsonfile), ":", "__"))
+    Returns:
+        str: the json. Keys contain : as namespace seperator
+    """
+    return json.dumps(to_dict(item), indent=2, default=serialize_datetime)
 
-def item_from_json(json_string:str)->Item:
-     """ load a json Item
 
-     Args:
-         json_string (str): the Item as json
+def item_from_json_file(jsonfile) -> Item:
+    """ load a json file containing an Item
 
-     Returns:
-         Item: the Item
-     """
-     return Item(**__replaceKeys(json.loads(json_string), ":", "__"))
+    Args:
+        jsonfile (file): the json file
 
-def item_from_dict(object:dict)->Item:
-     """ load a dict Item
+    Returns:
+        Item: _description_
+    """
+    return Item(**__replaceKeys(json.load(jsonfile), ":", "__"))
 
-     Args:
-         object (dict): the Item as dict
 
-     Returns:
-         Item: the Item
-     """
-     return Item(**__replaceKeys(object, ":", "__"))
+def item_from_json(json_string: str) -> Item:
+    """ load a json Item
 
-def __replaceKeys(o, before:str, after:str):
-     if type(o) is dict:
-          d={}
-          for key in o:
-               d[key.replace(before, after)]=__replaceKeys(o[key], before, after)
-          return d
-     if type(o) is list:
-          return list(map(lambda elt:__replaceKeys(elt, before, after), o))
-     else:
-          return o
+    Args:
+        json_string (str): the Item as json
+
+    Returns:
+        Item: the Item
+    """
+    return Item(**__replaceKeys(json.loads(json_string), ":", "__"))
+
+
+def item_from_dict(object: dict) -> Item:
+    """ load a dict Item
+
+    Args:
+        object (dict): the Item as dict
+
+    Returns:
+        Item: the Item
+    """
+    return Item(**__replaceKeys(object, ":", "__"))
+
+
+def __replaceKeys(o, before: str, after: str):
+    if type(o) is dict:
+        d = {}
+        for key in o:
+            d[key.replace(before, after)] = __replaceKeys(o[key], before, after)
+        return d
+    if type(o) is list:
+        return list(map(lambda elt: __replaceKeys(elt, before, after), o))
+    else:
+        return o
