24b37a12d155ac10a09f73e733252730125c1a38
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2024-10-23 16:14:45 +0200

    fix comments

--- agate/settings.py
+++ agate/settings.py
@@ -10,7 +10,7 @@ class Service(BaseModel, extra=Extra.allow):
     public_url_patterns: list[str] | None
     url_header: str
     url_header_prefix: str | None
-    pattern_target: str | None = Field(title="If undefined, then the pattern is matched against the path. User query.{param}, where {param} is the parameter value, to use a query parameter. Use query.{param}.url.path|query if the param value is a url and that you want to target the path or query of that url.")
+    pattern_target: str | None = Field(title="If undefined, then the pattern is matched against the path. Use query.{param}, where {param} is the parameter value, to use a query parameter. Use query.{param}.url.path|query if the param value is a url and that you want to target the path or query of that url.")
 
 
 class Settings(BaseModel, extra=Extra.allow):
@@ -18,7 +18,7 @@ class Settings(BaseModel, extra=Extra.allow):
     agate_prefix: str
     host: str
     port: int
-    services: dict[str, Service] = Field({}, title="dictionary of service name/values. Each value is a pattern configuration for extracting collection and id values for building an ARLAS request that determines whether the access is granted or not.")
+    services: dict[str, Service] = Field({}, title="Dictionary of service name/values. Each value is a pattern configuration for extracting collection and id values for building an ARLAS request that determines whether the access is granted or not.")
 
 
 class Configuration:
