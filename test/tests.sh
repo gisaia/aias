554c0424d516808c53f0e107ef7c4b082a7a4c3f
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2024-11-06 14:31:22 +0100

    fix: uncomment tests

--- test/tests.sh
+++ test/tests.sh
@@ -5,10 +5,10 @@ docker build -f Dockerfile-tests . -t pythontests
 
 # Set env variable
 . ./test/env.sh
-# docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m test.airs_tests
-# docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m test.aproc_ingest_tests
+docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m test.airs_tests
+docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m test.aproc_ingest_tests
 docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m test.aproc_download_tests
-# docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m test.agate_tests
-# docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m test.fam_tests
-#docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m  test.aproc_ingest_heavyload_tests
-# ./test/stop_stack.sh
+docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m test.agate_tests
+docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m test.fam_tests
+# docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m  test.aproc_ingest_heavyload_tests
+./test/stop_stack.sh
