3a6d004e70e6f711ad54bce969b97b956a835f3d
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2024-12-20 07:35:52 +0100

    fix mkdocs

--- mkDocs.sh
+++ mkDocs.sh
@@ -8,12 +8,13 @@ rm -rf target/generated-docs/*
 docker run -v `pwd`:/app  python:3 /bin/bash -c  "cd /app/; pip3 install pydantic ; python3 -m airs.core.models.utils > /app/docs/docs/model/model.schema.json"
 # Generate model documentation
 docker run --rm -v `pwd`/docs/docs/model:/schema/ gisaia/jsonschema2md:latest -d /schema/ -o /schema/ -x -
-sed -i'' 's/# /## /' docs/docs/model/model.md
+# sed -i'' 's/# /## /' docs/docs/model/model.md
 
 # Overwrite generated model README
-cat release/materials/README_head.md > docs/docs/model/README.md
 cat docs/docs/model/model.md >> docs/docs/model/README.md
 
 # Copy documentation to target
 cp -r docs/docs/* target/generated-docs/
-cp docs/docs/model/README.md target/README.md
+
+cat release/materials/README_head.md > target/README.md
+cat docs/docs/model/README.md >> target/README.md
