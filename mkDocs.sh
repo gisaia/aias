e76e911c6911602421660748779d2d1091068a64
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2025-03-10 11:13:26 +0100

    add agate doc

--- mkDocs.sh
+++ mkDocs.sh
@@ -4,25 +4,24 @@
 mkdir -p target/generated-docs
 rm -rf target/generated-docs/*
 
-# Generate AIRS docs
-mkdir -p docs/docs/api/airs
-mkdir -p docs/docs/api/fam
-mkdir -p docs/docs/api/aproc_service
-
 ## Run AIAS stack
 ./test/start_stack.sh
 
 ## Get AIRS api json file
-i=1; until curl -XGET http://0.0.0.0:8000/openapi.json -o docs/docs/airs/openapi.json; do if [ $i -lt 60 ]; then sleep 1; else break; fi; i=$(($i + 1)); done
+i=1; until curl -XGET http://localhost:8000/openapi.json -o docs/docs/airs/openapi.json; do if [ $i -lt 60 ]; then sleep 1; else break; fi; i=$(($i + 1)); done
 
 ## Get FAM api json file
-i=1; until curl -XGET http://0.0.0.0:8005/openapi.json -o docs/docs/fam/openapi.json; do if [ $i -lt 60 ]; then sleep 1; else break; fi; i=$(($i + 1)); done
+i=1; until curl -XGET http://localhost:8005/openapi.json -o docs/docs/fam/openapi.json; do if [ $i -lt 60 ]; then sleep 1; else break; fi; i=$(($i + 1)); done
 
-## Fet APROC service api file
-i=1; until curl -XGET http://0.0.0.0:8001/openapi.json -o docs/docs/aproc_service/openapi.json; do if [ $i -lt 60 ]; then sleep 1; else break; fi; i=$(($i + 1)); done
+## Get APROC service api file
+i=1; until curl -XGET http://localhost:8001/openapi.json -o docs/docs/aproc/openapi.json; do if [ $i -lt 60 ]; then sleep 1; else break; fi; i=$(($i + 1)); done
+
+## Get AGATE service api file
+i=1; until curl -XGET http://localhost:8004/openapi.json -o docs/docs/agate/openapi.json; do if [ $i -lt 60 ]; then sleep 1; else break; fi; i=$(($i + 1)); done
 
 ## Stop the AIAS stack
 ./test/stop_stack.sh
 
 # Copy documentation to target
 cp -r docs/docs/* target/generated-docs/
+mmdc -i target/generated-docs/agate/agate-mermaid.md -o target/generated-docs/agate/agate.md
