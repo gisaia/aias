33567540920c0e6bd510eb4f17239c016726114f
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2025-04-25 15:26:42 +0200

    dockle and hadolint in ci

--- release/quality.sh
+++ release/quality.sh
@@ -1,2 +1,11 @@
+set -o errexit
+[ -z "$1" ] && echo "Please provide the version" && exit 1;
 VERSION=$1
-dockle -af settings.py agate:${VERSION}
+
+dockle --exit-level WARN -af settings.py gisaia/agate:${VERSION}
+dockle -af settings.py gisaia/airs:${VERSION}
+dockle -af settings.py gisaia/fam:${VERSION}
+dockle -af settings.py -i CIS-DI-0009 -i DKL-DI-0005 gisaia/aproc-proc:${VERSION}
+dockle -af settings.py gisaia/aproc-service:${VERSION}
+dockle -af settings.py --accept-key KEY_SHA512 gisaia/arlas-fam-wui:${VERSION}
+dockle -af settings.py gisaia/stac-geodes:${VERSION}
