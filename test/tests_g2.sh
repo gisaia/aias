a215fcc745b961aba21b339910a0289ee93377a1
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2025-05-05 21:13:51 +0200

    Revert "first re-org"
    This reverts commit 15a503696a1d93545b73ebea5c3ad9ad7522df44.

--- test/tests_g2.sh
+++ test/tests_g2.sh
@@ -10,17 +10,17 @@ docker build -f Dockerfile-tests . -t pythontests
 docker network list
 
 echo "run test.airs_tests"
-docker run --rm -v `pwd`:/app/  --network compose_aias pythontests python3 -m test.airs_tests
+docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m test.airs_tests
 
 echo "run test.agate_tests"
-docker run --rm -v `pwd`:/app/  --network compose_aias pythontests python3 -m test.agate_tests
+docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m test.agate_tests
 
 echo "run test.fam_tests"
-docker run --rm -v `pwd`:/app/  --network compose_aias pythontests python3 -m test.fam_tests
+docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m test.fam_tests
 
 echo "run test.aproc_ingest_tests"
-docker run --rm -v `pwd`:/app/  --network compose_aias pythontests python3 -m test.aproc_ingest_tests
+docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m test.aproc_ingest_tests
 
 
-# docker run --rm -v `pwd`:/app/  --network compose_aias pythontests python3 -m  test.aproc_ingest_heavyload_tests
+# docker run --rm -v `pwd`:/app/  --network aias_aias pythontests python3 -m  test.aproc_ingest_heavyload_tests
 ./test/stop_stack.sh
