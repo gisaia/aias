241ec8f9d400ae70790da3a6cbf15471266fb921
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2025-06-11 16:42:03 +0200

    feat: allow openId auth mode

--- angular/file-explorer/src/app/app-routing.module.ts
+++ angular/file-explorer/src/app/app-routing.module.ts
@@ -19,19 +19,19 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuardIamService, ForgotComponent, LoginComponent, RegisterComponent, ResetComponent, VerifyComponent } from 'arlas-wui-toolkit';
+import { ForgotComponent, LoginComponent, RegisterComponent, ResetComponent, VerifyComponent } from 'arlas-wui-toolkit';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
-    path: '', component: HomeComponent, canActivate: [AuthGuardIamService],
+    path: '', component: HomeComponent,
   },
+  { path: 'callback', redirectTo: '' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'verify/:id/user/:token', component: VerifyComponent },
   { path: 'password_forgot', component: ForgotComponent },
   { path: 'reset/:id/user/:token', component: ResetComponent },
-  // otherwise redirect to login
   { path: '**', redirectTo: '' }
 ];
 
--- angular/file-explorer/src/app/components/home/home.component.ts
+++ angular/file-explorer/src/app/components/home/home.component.ts
@@ -17,10 +17,9 @@
  * under the License.
  */
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FamService } from '@services/fam/fam.service';
 import { JobService } from '@services/job/job.service';
-import { ArlasIamService } from 'arlas-wui-toolkit';
 import { Subject } from 'rxjs';
 
 @Component({
@@ -28,29 +27,18 @@ import { Subject } from 'rxjs';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   public archivesPath = '';
   public collapseEvent: Subject<boolean> = new Subject();
   public refreshTasks: Subject<boolean> = new Subject();
   public showTasks = true;
 
-  public userEmail!: any;
-  public isMenuOpen = false;
 
   constructor(
-    private arlasIamService: ArlasIamService,
     private famService: FamService,
     private jobsService: JobService
   ) { }
-
-  ngOnInit(): void {
-    this.userEmail = this.arlasIamService.user.email;
-  }
-
-  public logout() {
-    this.arlasIamService.logout();
-  }
 
   public refresh() {
     this.famService.refreshArchives$.next(true);
