e3de6a8957563a4affa1a0946e81a9aeef6217ae
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2025-06-16 09:39:03 +0200

    initial round for doc

--- python/agate/settings.py
+++ python/agate/settings.py
@@ -8,32 +8,32 @@ LOGGER = Logger.logger
 
 
 class Service(BaseModel, extra=Extra.allow):
-    url_patterns: list[str]
-    public_url_patterns: list[str] | None
-    url_header: str
-    url_header_prefix: str | None
+    url_patterns: list[str] = Field(title="Pattern list", description="List of patterns for the service for extracting the collection name and item name (e.g. (?P<collection>[^/]+)/items/(?P<item>[^/]+)/assets/overview)")
+    public_url_patterns: list[str] | None = Field(title="Public pattern list", description="List of patterns for public access. IMPORTANT: no access control done on those patterns.")
+    url_header: str = Field(title="URL Header", description="Header containing the URL")
+    url_header_prefix: str | None = Field(title="URL Prefix", description="URL prefix that is removed before pattern matching.")
     pattern_target: str | None = Field(title="If undefined, then the pattern is matched against the path. Use query.{param}, where {param} is the parameter value, to use a query parameter. Use query.{param}.url.path|query if the param value is a url and that you want to target the path or query of that url.")
 
 
 class URBAC(BaseModel, extra=Extra.allow):
-    url_header: str
-    method_header: str
-    jwt_header: str
-    roles: Roles = Roles(technicalRoles={})
-    role_file: str
+    url_header: str = Field(title="URL Header", description="The header containing the requested URL")
+    method_header: str = Field(title="Method header", description="The header containing the method")
+    jwt_header: str = Field(title="JWT Header", description="The header containing the JWT (can start with 'Bearer')")
+    roles: Roles = Field(default=Roles(technicalRoles={}), title="Roles", description="Definition of the endpoints and of the authorized roles. This is automatically filled from role_file")
+    role_file: str = Field(title="Role file", description="File location containing the roles")
 
 
 class Settings(BaseModel, extra=Extra.allow):
-    arlas_url_search: str = Field(title="ARLAS URL Search (ex http://arlas-server:9999/arlas/explore/{collection}/_search?f=id:eq:{item})")
-    agate_prefix: str
-    host: str
-    port: int
-    services: dict[str, Service] = Field({}, title="Dictionary of service name/values. Each value is a pattern configuration for extracting collection and id values for building an ARLAS request that determines whether the access is granted or not.")
-    urbac: URBAC
+    arlas_url_search: str = Field(title="ARLAS URL Search", description="ARLAS URL Search (ex http://arlas-server:9999/arlas/explore/{collection}/_search?f=id:eq:{item})")
+    agate_prefix: str = Field(title="Agate endpoint prefix", description="Agate endpoint prefix")
+    host: str = Field(title="Host", description="Agate service connection host")
+    port: int = Field(title="Port", description="Agate service port")
+    services: dict[str, Service] = Field({}, title="Services to protect", description="Dictionary of service name/definition. A service protects an endpoint that is exposing resources linked to an ARLAS Item. The service definition tells how to extract the collection name and the item id that are then used for checking with ARLAS whether the item is accessible or not.")
+    urbac: URBAC = Field(title="URBAC definition", description="URL Role Based Access Control Definition. A user access an endpoint if one of his role is configured in the role configuration file for the requested endpoint (roles.yaml)")
 
 
 class Configuration:
-    settings: Settings = None
+    settings: Settings = Field(title="Agate configuration", description="Configuration of ARLAS Gateway for Assets.")
 
     @staticmethod
     def init(configuration_file: str):
--- python/aproc/core/settings.py
+++ python/aproc/core/settings.py
@@ -8,21 +8,21 @@ LOGGER = Logger.logger
 
 
 class ProcessSettings(BaseModel, extra='allow'):
-    name: str | None = Field(title="Name of the process")
-    class_name: str | None = Field(title="Name of the process class")
-    configuration: dict | None = Field(title="Process configuration")
+    name: str | None = Field(title="Process Name", description="Name of the process")
+    class_name: str | None = Field(title="Process class", description="Name of the process class")
+    configuration: dict | None = Field(title="Process configuration", description="Configuration that is specific the process (dictionary key/value)")
 
 
 class Settings(BaseModel, extra='allow'):
-    celery_broker_url: str | None = Field(title="Celery's broker url")
-    celery_result_backend: str | None = Field(title="Celery's backend")
-    processes: list[ProcessSettings] = Field(title="List of processes available")
-    airs_endpoint: str | None = Field(title="ARLAS Item Registration Service endpoint")
-    access_manager: AccessManagerSettings = Field(title="Configuration for the AccessManager")
+    celery_broker_url: str | None = Field(title="Celery's broker url", description="Celery's broker url of the form of transport://userid:password@hostname:port/virtual_host")
+    celery_result_backend: str | None = Field(title="Celery's backend", description="Celery's backend used to store task results")
+    processes: list[ProcessSettings] = Field(title="List of processes", description="List of APROC processes")
+    airs_endpoint: str | None = Field(title="AIRS endpoint", description="ARLAS Item Registration Service endpoint")
+    access_manager: AccessManagerSettings = Field(title="AccessManager configuration", description="Configuration for the AccessManager")
 
 
 class Configuration:
-    settings: Settings | None = Field(title="aproc Service configuration")
+    settings: Settings | None = Field(title="APROC Service configuration", description="The configuration of the APROC Service")
 
     @staticmethod
     def init(configuration_file: str):
