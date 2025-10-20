d9ca8ed010dedba26752abbc5c353978c3fbdfc9
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2025-10-20 09:01:45 +0200

    openid uri->jwks uri

--- python/agate/rest/authorizations.py
+++ python/agate/rest/authorizations.py
@@ -13,36 +13,27 @@ class Authorizations:
     keys: dict = {}
 
     @staticmethod
-    def load_keys_from_openid_configuration(uri: str, verify_ssl: bool = True):
-        """ Retrieve the JWKS from the OpenID configuration located at uri and return a dictionary of keys indexed by their kid.
+    def load_keys_from_jwks_uri(jwks_uri: str, verify_ssl: bool = True):
+        """ Retrieve the JWKS from the uri and return a dictionary of keys indexed by their kid.
 
         Args:
-            uri (str): The OpenID configuration uri (e.g. https://accounts.google.com/.well-known/openid-configuration)
+            jwks_uri (str): The JWKS uri (e.g. https://www.googleapis.com/oauth2/v3/certs)
 
         Raises:
-            Exception: If the OpenID configuration or the JWKS can not be retrieved.
+            Exception: If the JWKS can not be retrieved.
 
         Returns:
             dict: A dictionary of keys indexed by their kid.
         """
-        LOGGER.debug(f"Retrieving OpenID configuration from {uri}")
-        r = requests.get(uri, verify=verify_ssl)
+        r = requests.get(jwks_uri, verify=verify_ssl)
         if r.status_code == 200:
-            jwks_uri = r.json().get("jwks_uri")
-            if jwks_uri:
-                r = requests.get(jwks_uri, verify=verify_ssl)
-                if r.status_code == 200:
-                    jwks = r.json()
-                    LOGGER.debug(f"Loading JWKS from {jwks_uri}")
-                    from jwt.algorithms import RSAAlgorithm
-                    for e in jwks.get("keys", []):
-                        Authorizations.keys[e['kid']] = RSAAlgorithm.from_jwk(e)
-                else:
-                    raise Exception(f"Unable to retrieve JWKS from {jwks_uri}, status code {r.status_code}")
-            else:
-                raise Exception(f"No jwks_uri found in the OpenID configuration from {uri}")
+            jwks = r.json()
+            LOGGER.debug(f"Loading JWKS from {jwks_uri}")
+            from jwt.algorithms import RSAAlgorithm
+            for e in jwks.get("keys", []):
+                Authorizations.keys[e['kid']] = RSAAlgorithm.from_jwk(e)
         else:
-            raise Exception(f"Unable to retrieve JWKS from {uri}, status code {r.status_code}")
+            raise Exception(f"Unable to retrieve JWKS from {jwks_uri}, status code {r.status_code}")
 
     @staticmethod
     def get_user_roles(authorization: str) -> list[str]:
