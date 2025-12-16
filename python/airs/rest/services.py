e11f47f38d55d1c98c7c8ab690053fa0c24b792c
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2025-12-16 16:44:44 +0100

    remove unusable link

--- python/airs/rest/services.py
+++ python/airs/rest/services.py
@@ -29,17 +29,7 @@ async def init_collection(collection: str, request: Request) -> JSONResponse:
             response_model=CollectionDescriptionListResponse,
             response_model_exclude_none=True)
 async def list_collection(request: Request) -> CollectionDescriptionListResponse:
-    collections = rs.list_collections()
-    for c in collections.collections:
-        c.links = [
-            Link(
-                href=str(request.base_url) + "collections/" + c.id,
-                rel="self",
-                type="application/json",
-                title=f"Collection {c.id}"
-            )
-        ]
-    return collections
+    return rs.list_collections()
 
 
 @ROUTER.post('/collections/{collection}/items', description="Create an item. item.id must be set. Asset must exist (depends on the server configuration)",
