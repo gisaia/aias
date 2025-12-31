aa10f2c2218352265f525a7beda27eda8441b6d4
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2025-12-31 08:27:21 +0100

    fix model for include driver path request of fam

--- python/fam/core/model.py
+++ python/fam/core/model.py
@@ -6,7 +6,7 @@ from aias_common.access.file import File
 class PathRequest(BaseModel):
     path: str = ""
     size: int = 10
-    include_drivers: list[str]
+    include_drivers: list[str] = []
 
 
 class Archive(File):
