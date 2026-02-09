7cf5af50240feb621e7984a35e1f2bac65c9242c
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2026-02-09 10:11:38 +0100

    upgrade versions

--- test/stop_stack.sh
+++ test/stop_stack.sh
@@ -2,3 +2,4 @@ source ./test/env.sh
 docker compose -f docker-compose-tests.yaml down
 docker compose -f docker-compose.yaml down
 rm -rf ./outbox
+docker rm airs-server rabbitmq redis fam-service elasticsearch smtp4dev minio agate aproc-service aproc-processes
