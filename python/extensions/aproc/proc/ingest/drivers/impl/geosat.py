1feb6c1a25a785856375dd30735e428450300c3f
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2026-02-20 16:28:04 +0100

    build overview if local

--- python/extensions/aproc/proc/ingest/drivers/impl/geosat.py
+++ python/extensions/aproc/proc/ingest/drivers/impl/geosat.py
@@ -58,7 +58,7 @@ class Driver(IngestDriver):
 
     # Implements drivers method
     def transform_assets(self, url: str, assets: list[Asset]) -> list[Asset]:
-        if self.quicklook_path is None:
+        if self.quicklook_path is None and AccessManager.is_local(self.tif_path):
             quicklook = ImageDriverHelper.prepare_preview_asset(self, url, Role.overview, MimeType.JPG, AssetFormat.jpg)
             geotiff_to_jpg(self.tif_path, Driver.OVERVIEW_FROM_TIFF_PCT, Driver.OVERVIEW_FROM_TIFF_PCT, output_path=quicklook.href, bands_list=[1, 2, 3])
             quicklook.size = AccessManager.get_size(quicklook.href)
