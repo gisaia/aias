3bd9408ee797b8a20be3d725eca44e91fd06e63c
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2026-07-02 15:45:23 +0200

    remove unused test

--- test/aproc_ingest_tests_gs.py
+++ test/aproc_ingest_tests_gs.py
@@ -195,9 +195,5 @@ class Tests(IngestTests):
         url = os.path.join(ROOT, SUPERVIEW3_4 + "_PAN")
         self.async_ingest(url, ["thumbnail", "overview", "data", "metadata", "airs_item", Role.archive.value, Role.pan.value, Role.overview.value + "-pan", Role.rpc.value + "-pan", Role.metadata.value + "-pan"])
 
-    def test_async_ingest_superview_gaofen_pan(self):  # Driver SUPERVIEW
-        url = os.path.join(ROOT, SUPERVIEW_GAOFEN)
-        self.async_ingest(url, ["thumbnail", "overview", "data", "metadata", "airs_item", Role.archive.value, Role.pan.value, Role.overview.value + "-pan", Role.rpc.value + "-pan", Role.metadata.value + "-pan"])
-
 if __name__ == '__main__':
     unittest.main()
