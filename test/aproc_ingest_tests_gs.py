2160f6f1e3470b446a7187f38825fecff0cd1512
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2026-07-02 15:45:46 +0200

    remove unused test

--- test/aproc_ingest_tests_gs.py
+++ test/aproc_ingest_tests_gs.py
@@ -4,7 +4,7 @@ from airs.core.models.model import Role
 from aproc.core.models.ogc.enums import StatusCode
 
 from test.aproc_ingest_tests import (AST, CAPELLA1, CAPELLA2, CAPELLA3, CSK, CSK2, SPOT6, GEOSAT, ICEYE, IKONOS, JP2000, PNEOMS, PNEOPAN,
-                                     RADARSAT2, RAPID_EYE, SATELLOGIC, SENTINEL1_GRDH, SUPERVIEW, SUPERVIEW3_4, SUPERVIEW_GAOFEN, WYVERN, LANDSAT9,
+                                     RADARSAT2, RAPID_EYE, SATELLOGIC, SENTINEL1_GRDH, SUPERVIEW, SUPERVIEW3_4, WYVERN, LANDSAT9,
                                      SENTINEL1_SLC, SENTINEL2, SKYSAT, SPOT5,
                                      TERRASARX, TIF, WORLDVIEW, UMBRA_STAC, IngestTests)
 from test.utils import CATALOG, COLLECTION, SENTINEL2_BANDS
