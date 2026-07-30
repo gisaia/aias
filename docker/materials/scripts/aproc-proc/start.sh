e88b6088d2017ed951c681d653a42015f347ba54
MatthieuBarbet <matthieu.barbet@gisaia.com>
2026-07-30 16:27:41 +0800

    Fix: aproc-proc start.sh venv folder

--- docker/materials/scripts/aproc-proc/start.sh
+++ docker/materials/scripts/aproc-proc/start.sh
@@ -10,5 +10,5 @@ else
     echo "Starting worker for default queue: $QUEUE_NAMES"
 fi
 
-. aproc/bin/activate
+. /opt/aproc/bin/activate
 celery -A aproc.core.processes.processes:APROC_CELERY_APP worker -Q ${QUEUE_NAMES} -E -c 1 -n worker@%h $*
