8fce5dbe4a8f7b9a5ac9105c6b0aef875bc9a558
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2026-08-18 19:05:59 +0200

    remove test that can run only with gs

--- test/fam_gs_tests.py
+++ test/fam_gs_tests.py
@@ -9,19 +9,5 @@ class Tests(FAMTests):
     def setUp(self):
         FAMTests.URL = "http://fam-gs-service:8005/arlas/fam"  # NOSONAR
 
-    def test_identifier_are_the_same(self):
-        r = requests.get(url="/".join([Tests.URL, "root"]))
-        root: File = File(**r.json())
-        r = requests.post(url="/".join([Tests.URL, "archives"]), data=PathRequest(path="/".join([root.path, "spacewill/superview"])).model_dump_json(), headers={"Content-Type": "application/json"})
-        self.assertTrue(r.ok, str(r.status_code) + ": " + str(r.content))
-        print(r.json())
-        name_to_id = {}
-        for item in r.json():
-            name_to_id[item["name"]] = item["id"]
-        r = requests.post(url="/".join([Tests.URL, "archives"]), data=PathRequest(path="/".join([root.path, "spacewill/superview/SPACEWILL_SUPERVIEW_TIFF_MANUAL_TASKING_SYNTHETIC"])).model_dump_json(), headers={"Content-Type": "application/json"})
-        for item in r.json():
-            self.assertEqual(item["id"], name_to_id[item["name"]])
-
-
 if __name__ == '__main__':
     unittest.main()
