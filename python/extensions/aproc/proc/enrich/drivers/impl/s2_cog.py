881b50c528c127083fb6ee237bac11218c30d0be
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2026-09-15 14:17:35 +0200

    remove resolution:highest

--- python/extensions/aproc/proc/enrich/drivers/impl/s2_cog.py
+++ python/extensions/aproc/proc/enrich/drivers/impl/s2_cog.py
@@ -148,7 +148,7 @@ class Driver(EnrichDriver):
             with AccessManager.make_local_list(band_files) as local_assets:
 
                 # Build VRT to facilitate COG built
-                kwargs = {"separate": True, "resolution": "highest"}
+                kwargs = {"separate": True}
                 gdal.BuildVRT(source_files_vrt, local_assets, **kwargs)
                 all_bands_cog_max_width_or_height = Driver.configuration['all_bands_cog_max_width_or_height']
                 CogBuilderHelper.build(source_files_vrt, target_asset_location, max_px_width_or_height=all_bands_cog_max_width_or_height)
